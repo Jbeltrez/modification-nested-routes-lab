cdf4dfd1af09b66120b8df801dfe42ddcc3a1f76
Jbeltrez <beltrezjohn@gmail.com>
2021-04-14 18:04:32 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
+    # if were editing straight from non nested
+    # resource, create this helper to allow us 
+    # to select an artist from a drop down 
+    # if were in the nested route under artists 
+    # produce a hidden_field_tag so that we can send over 
+    # artist_id params which would have existed from the 
+    # controller - remember babysitting that value. 
 
 end
